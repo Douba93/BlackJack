e1eddeb627719d403e060505f8e52c138c379736
Douba93 <mmestron@yahoo.com>
2023-05-21 20:50:53 +0200

    [add]: +++

--- js/blackjack.js
+++ js/blackjack.js
@@ -14,6 +14,15 @@ class Deck {
     }
 
     dealCard() {
+        if (this.deck.length > 15) {
+            this.deck = [
+                "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
+                "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
+                "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
+                "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS"
+            ];
+            this.shuffle();
+        }
         return this.deck.pop();
     }
 }
@@ -21,6 +30,11 @@ class Deck {
 class Player {
     constructor() {
         this.hand = []; // Main du joueur, initialisée comme une liste vide
+        this.wallet = 500;
+    }
+
+    bet(dols) {
+        this.wallet = this.wallet - dols;
     }
 
     drawCard(card) {
@@ -93,54 +107,112 @@ class Game {
         this.deck = new Deck();
         this.player = new Player();
         this.bank = new Bank();
-
+        this.pot = 0;
     }
 
     initGame() {
-        this.start();
-        this.setPlayerScore(this.player.calculateScore());
-        this.setBankScore(this.bank.calculateScore());
+        this.showWallet();
+        this.showPot();
 
         let reloadBtn = document.querySelector('#reload');
+        let hit = document.querySelector('#hit');
+        let stand = document.querySelector('#stand');
+        let betBtn = document.querySelector('#betBtn');
+        let bet = document.querySelector('#bet');
+
         reloadBtn.disabled = true;
         reloadBtn.style.visibility = 'hidden';
 
-        document.querySelector('#hit').addEventListener('click', () => {
-            this.hit();
-            this.setPlayerScore(this.player.calculateScore());
-        });
-        document.querySelector('#stand').addEventListener('click', () => {
-            this.bankTurn();
-            this.setBankScore(this.bank.calculateScore());
-        });
-        reloadBtn.addEventListener('click', (e) => {
-            let finalResult = document.querySelector('#finalResult');
-            let hit = document.querySelector('#hit');
-            let stand = document.querySelector('#stand');
+        hit.disabled = true;
+        hit.style.visibility = 'hidden';
 
-            finalResult.innerHTML = '';
-            stand.disabled = false;
-            hit.disabled = false;
+        stand.disabled = true;
+        stand.style.visibility = 'hidden';
+
+        betBtn.addEventListener('click', (e) => {
+            this.player.bet(bet.value);
+            this.pot = bet.value;
             e.target.disabled = true;
             e.target.style.visibility = 'hidden';
+            bet.disabled = true;
+            bet.style.visibility = 'hidden';
 
-            this.player.hand = [];
-            this.bank.hand = [];
-            this.showHand();
+            hit.disabled = false;
+            hit.style.visibility = 'visible';
+
+            stand.disabled = false;
+            stand.style.visibility = 'visible';
+
+            this.showWallet();
+            this.showPot();
+
             this.start();
             this.setPlayerScore(this.player.calculateScore());
             this.setBankScore(this.bank.calculateScore());
+
+        });
+
+        hit.addEventListener('click', () => {
+            this.hit();
+            this.setPlayerScore(this.player.calculateScore());
+        });
+
+        stand.addEventListener('click', () => {
+            this.bankTurn();
+            this.setBankScore(this.bank.calculateScore());
+        });
+
+        reloadBtn.addEventListener('click', (e) => {
+            let finalResult = document.querySelector('#finalResult');
+            let playerScore = document.querySelector('#playerScore');
+            let bankScore = document.querySelector('#bankScore');
+            let bankHand = document.querySelector('#bankHand');
+            let playerHand = document.querySelector('#playerHand');
+
+            finalResult.innerHTML = '';
+            playerScore.innerHTML = '';
+            bankScore.innerHTML = '';
+            playerHand.innerHTML = '';
+            bankHand.innerHTML = '';
+
+            e.target.disabled = true;
+            e.target.style.visibility = 'hidden';
+
+            betBtn.disabled = false;
+            betBtn.style.visibility = 'visible';
+
+            bet.disabled = false;
+            bet.style.visibility = 'visible';
+
+            hit.disabled = true;
+            hit.style.visibility = 'hidden';
+
+            stand.disabled = true;
+            stand.style.visibility = 'hidden';
+
+            this.player.hand = [];
+            this.bank.hand = [];
+            this.pot = 0;
+
+            this.showWallet();
+            this.showPot();
+            this.showHand();
+            this.showBankHand();
         });
     }
 
-    start() {
+    async start() {
         this.deck.shuffle();
 
         this.player.drawCard(this.deck.dealCard());
         this.bank.drawCard(this.deck.dealCard());
         this.player.drawCard(this.deck.dealCard());
 
-        this.showHand(); // Affiche la main du joueur
+        await this.showHand(); // Affiche la main du joueur
+        await this.showBankHand();
+
+        this.setPlayerScore(this.player.calculateScore());
+        this.setBankScore(this.bank.calculateScore());
     }
 
     // 0 = rester | 1 = tirer une carte
@@ -153,29 +225,57 @@ class Game {
         }
     }
 
-    showHand() {
+    showPot() {
+        let pot = document.querySelector('#pot');
+        pot.innerHTML = `pot: ${this.pot}`;
+    }
 
+    showWallet() {
+        let playerWallet = document.querySelector('#playerWallet');
+        playerWallet.textContent = `Wallet : ${this.player.wallet}`;
+    }
+
+    async showHand() {
         let playerHand = document.querySelector('#playerHand');
-        let bankHand = document.querySelector('#bankHand');
 
-        playerHand.innerHTML = '';
-        bankHand.innerHTML = '';
-
-        for (let card of this.player.hand) {
-            playerHand.innerHTML += `
-                <img class='cards' src='./assets/png/${card}.png1' alt='${card}'/>
+        this.player.hand.forEach((card, index) => {
+            if (!document.querySelector(`#card_${card}`)) {
+                playerHand.innerHTML += `
+              <img id='card_${card}' class='cards card-animation' src='./assets/png/blue_back.png1' alt='${card}'/>
             `;
-        }
-
-        for (let card of this.bank.hand) {
-            playerHand.innerHTML += `
-                <img class='cards' src='./assets/png/${card}.png1' alt='${card}'/>
-            `;
-        }
+            }
+            this.animateCard(card, index);
+        });
 
         if (this.player.calculateScore() == 21) {
             this.bankTurn();
         }
+    }
+
+    async showBankHand() {
+        let bankHand = document.querySelector('#bankHand');
+
+        this.bank.hand.forEach((card, index) => {
+            if (!document.querySelector(`#card_${card}`)) {
+                bankHand.innerHTML += `
+              <img id='card_${card}' class='cards card-animation' src='./assets/png/blue_back.png1' alt='${card}'/>
+            `;
+            }
+            this.animateCard(card, index, bankHand);
+        });
+    }
+
+    animateCard(card, index, targetElement) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                const cardElement = document.querySelector(`#card_${card}`);
+                if (!cardElement.classList.contains('active')) {
+                    cardElement.classList.add('active');
+                    cardElement.src = `./assets/png/${card}.png1`;
+                }
+                resolve();
+            }, 500 * index);
+        });
     }
 
     hit() {
@@ -195,7 +295,7 @@ class Game {
     bankTurn() {
         while (this.bank.calculateScore() < 17) {
             this.bank.drawCard(this.deck.dealCard());
-            this.showHand();
+            this.showBankHand();
         }
         this.end();
     }
@@ -209,52 +309,60 @@ class Game {
     }
 
     end() {
-        let finalResult = document.querySelector('#finalResult');
-        let hit = document.querySelector('#hit');
-        let stand = document.querySelector('#stand');
-        let reloadBtn = document.querySelector('#reload');
+        return new Promise(resolve => {
+            let finalResult = document.querySelector('#finalResult');
+            let hit = document.querySelector('#hit');
+            let stand = document.querySelector('#stand');
+            let reloadBtn = document.querySelector('#reload');
 
-        reloadBtn.disabled = false;
-        reloadBtn.style.visibility = 'visible';
+            reloadBtn.disabled = false;
+            reloadBtn.style.visibility = 'visible';
 
-        if (this.bank.calculateScore() > 21) {
-            finalResult.innerHTML = `
-                <h1>Le joueur a gagner</h1>
-            `;
-            hit.disabled = true;
-            stand.disabled = true;
-        } else {
-            if (this.player.calculateScore() > 21) {
+            if (this.bank.calculateScore() > 21) {
+                this.player.wallet += this.pot * 2;
                 finalResult.innerHTML = `
-                <h1>La banque a gagner</h1>
-            `;
+                    <h1>Le joueur a gagné</h1>
+                `;
                 hit.disabled = true;
                 stand.disabled = true;
             } else {
-                if (this.player.calculateScore() == this.bank.calculateScore()) {
+                if (this.player.calculateScore() > 21) {
                     finalResult.innerHTML = `
-                        <h1>EGALITE</h1>
+                        <h1>La banque a gagné</h1>
                     `;
                     hit.disabled = true;
                     stand.disabled = true;
-                }
-                if (this.player.calculateScore() > this.bank.calculateScore()) {
-                    finalResult.innerHTML = `
-                        <h1>Le joueur a gagner</h1>
-                    `;
-                    hit.disabled = true;
-                    stand.disabled = true;
-                }
-                if (this.player.calculateScore() < this.bank.calculateScore()) {
-                    finalResult.innerHTML = `
-                        <h1>La banque a gagner</h1>
-                    `;
-                    hit.disabled = true;
-                    stand.disabled = true;
+                } else {
+                    if (this.player.calculateScore() == this.bank.calculateScore()) {
+                        this.player.wallet += this.pot;
+                        finalResult.innerHTML = `
+                            <h1>ÉGALITÉ</h1>
+                        `;
+                        hit.disabled = true;
+                        stand.disabled = true;
+                    } else if (this.player.calculateScore() > this.bank.calculateScore()) {
+                        this.player.wallet += this.pot * 2;
+                        finalResult.innerHTML = `
+                            <h1>Le joueur a gagné</h1>
+                        `;
+                        hit.disabled = true;
+                        stand.disabled = true;
+                    } else {
+                        finalResult.innerHTML = `
+                            <h1>La banque a gagné</h1>
+                        `;
+                        hit.disabled = true;
+                        stand.disabled = true;
+                    }
                 }
             }
-        }
+
+            this.setPlayerScore(this.player.calculateScore());
+            this.setBankScore(this.bank.calculateScore());
+        });
     }
+
+
 }
 
 const game = new Game();
